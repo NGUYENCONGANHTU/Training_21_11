d48c3907a5881e021f6c5cac62c0ad073a6f9b46
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2023-11-21 14:01:57 +0700

    push

---
+++ fix_profile/js.js
@@ -0,0 +1,29 @@
+var userID = document.getElementById('userID').value;
+var email = document.getElementById('email').value;
+var password = document.getElementById('password').value;
+var errorMessage = document.getElementById('error-message');
+
+function validateLogin(){
+    if (!/^[A-Za-z0-9]{6,}$/.test(userID)) {
+        errorMessage.innerHTML = 'UserID không hợp lệ';
+        errorMessage.style.color = "red";
+        return;
+      }
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      errorMessage.innerHTML = 'Email không hợp lệ.';
+      errorMessage.style.color = "red";
+      return;
+    }
+    var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,12}$/;
+         if (!passwordRegex.test(password)) {
+        errorMessage.innerHTML = 'Mật khẩu không đáp ứng yêu cầu.';
+        errorMessage.style.color = "red";
+         return;
+    }
+
+    errorMessage.innerHTML='';
+    alert('Đăng nhập thành công!');
+}
+
+
